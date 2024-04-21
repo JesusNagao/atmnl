4db20f25aea7ee2f65899b84a868565653758f58
Leemorand1410 <leemorand1410@gmail.com>
2024-04-21 00:52:20 -0600

    Andrew Update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './Header.css';
+import './Footer.css';
 
-export default function Header(){
+export default function Footer(){
     return( 
-        <div className='header-background'>
-            <img className='ATMNL_Logo' src = "/images/AT.png" alt = "ATMNL Logo"></img>
-            <div className='header-content'>
-                <h1 className='header-title'>Asociacion de Tenis de Mesa de Nuevo Leon</h1>
+        <div className='Footer'>
+            <div className='Footer-content'>
+                <h1 className='Footer-title'>Asociacion de Tenis de Mesa de Nuevo Leon</h1>
                 <div className='header-nav'>
                     <a href="/Nosotros" className='Nav'>Acerca de Nosotros</a> {/*Es una de las pestañas más importantes para este sprint*/}
                     <a href="/noticias" className='Nav'>Ultimas Noticias</a> {/*Es una de las pestañas más importantes para este sprint*/}
@@ -16,6 +15,7 @@ export default function Header(){
                     
                     {/* Más elementos de menú según sea necesario, posiblemente necesario*/}
                 </div>
+                <br></br>
             </div>
         </div>
     );
