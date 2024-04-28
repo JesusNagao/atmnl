a3da9f7c915bb20b13ecddbd840d08f4653ee189
Jesus Nagao <jnagaoalvarez@gmail.com>
2024-04-27 18:22:22 -0600

    Start Backend Implementation

--- server/index.js
+++ server/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const sql = require('mysql');
+require('dotenv').config()
 
 const app = express();
-
 const port = process.env.PORT || 3000;
+
+const connection = sql.createConnection({
+    host: process.env.SERVER,
+    user: process.env.admin,
+    password: process.env.pass,
+    database: process.env.db
+});
+
+connection.connect((err) => {
+    if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+    }
+    console.log('Connected to the database!');
+   });
+
 
 app.use(express.static("build"));
 
