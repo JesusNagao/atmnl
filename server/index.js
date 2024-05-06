df2a3e4a3c9cbbbee93e204f5e2ccb35625fa8b6
Jesus Nagao <jnagaoalvarez@gmail.com>
2024-05-05 19:58:37 -0600

    Update

--- server/index.js
+++ server/index.js
@@ -1,14 +1,7 @@
-//const { eventWrapper } = require('@testing-library/user-event/dist/utils');
 const express = require('express');
-//require('dotenv').config()
 const md5 = require('md5');
 const sql = require('mssql');
 
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(express.static("build"));
 
 var config = {
   "user": process.env.admin_user, // Database username
@@ -28,25 +21,19 @@ sql.connect(config, err => {
 });
 
 
-app.get('/hello', (req, res) =>{
-  res.send('Hello World')
-});
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.post('/hello', (req, res) => {
-  new sql.Request().query("SELECT * FROM tbl_News", (err, result) => {
-    if (err) {
-        console.error("Error executing query:", err);
-    } else {
-        res.send(result.recordset); // Send query result as response
-        //console.dir(result.recordset);
-        console.log("Query Successful")
-    }
-  });
-
-  //res.send("Query Successful")
-});
+app.use(express.static("build"));
 
 app.post('/newClub', (req, res) => {
+  sql.connect(config, err => {
+    if (err) {
+        throw err;
+    }
+    console.log("Connection Successful!");
+  });
+  
   var nameval = "\'TT con Orden\'";
   //var lat = 1.0
   //var lng = 1.0
@@ -67,6 +54,25 @@ app.post('/newClub', (req, res) => {
   });
 });
 
+app.get('/signup', (req, res) =>{
 
+  var query = "SELECT [name] FROM [atmnldev].[dbo].[tbl_Club];"
+  console.log(dbconnection(query));
+});
 
 app.listen(port, () => console.log("Server Started"));
+
+
+async function dbconnection(query){
+
+  await new sql.Request().query(query, (err, result) => {
+    if (err) {
+      console.error("Error executing query:", err);
+      return("[]")
+    }else{
+      //console.log(result.recordset)
+      return(result.recordset); // Send query result as response
+    }
+  });
+
+}
---
+++ src/components/Dynamic Components/SignUp.js
@@ -0,0 +1,11 @@
+export default function SignUp(){
+    return(
+        <div className="signup">
+
+            <form action="/signup" method="POST">
+                <label></label>
+            </form>
+
+        </div>
+    );
+}
