c8f91e2780fc2cf96063116a4e69621d6e90fd56
Leemorand1410 <leemorand1410@gmail.com>
2025-04-12 21:32:40 -0600

    Refactor: Enhance About Us and Events components with improved layout and styles; update routing and add new image assets

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,10 @@
-import Home from '../components/DynamicComponents/MainPage/MainPage';
+import Home from '../components/DynamicComponents/Main Page/MainPage';
 import About from '../components/StaticComponents/AboutUs/AboutUs';
 import Events from '../components/StaticComponents/Events/Events';
 
 const routes = [
   { path: '/', name: 'Inicio', element: <Home /> },
   { path: '/federacion', name: 'Sobre Nosotros', element: <About /> },
-  
   { path: '/calendario', name: 'calendario', element: <Events /> },
   // Add more as needed
 ];
