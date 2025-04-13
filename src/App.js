1fa000b0163483489bd5760c630ec61a0b475322
Leemorand1410 <leemorand1410@gmail.com>
2025-04-12 18:04:56 -0600

    Refactor: Remove unused Header and Footer components, add Login and MainPage components
    - Deleted Footer.js and Footer.css as they were no longer needed.
    - Removed Header.js and Header.css to streamline the component structure.
    - Introduced Login.js and Login.css for user authentication functionality.
    - Added MainPage.js and MainPage.css to serve as the main content area.
    - Implemented News, Ranking, and Videos components within MainPage.
    - Created AboutUs component with team information and mission statement.
    - Established Events component to display a Google Calendar for upcoming events.
    - Added styled-components for BurgerButton to enhance navigation.
    - Updated CSS styles across various components for improved layout and design.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import Header from './components/Static Components/Header/Header'
-import Footer from './components/Static Components/Footer/Footer'
+import Header from './components/StaticComponents/Header/Header'
+import Footer from './components/StaticComponents/Footer/Footer'
 //import Events from './components/Static Components/Events'
 
 
-import UploadNews from './components/Dynamic Components/Upload News/UploadNews'
+import UploadNews from './components/DynamicComponents/Upload News/UploadNews'
 import './App.css';
 
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-const Events = React.lazy(() => import('./components/Static Components/Events/Events'))
-const MainPage = React.lazy(() => import('./components/Dynamic Components/Main Page/MainPage')) 
-const AboutUs = React.lazy(() => import('./components/Static Components/About Us/AboutUs')) 
+const Events = React.lazy(() => import('./components/StaticComponents/Events/Events'))
+const MainPage = React.lazy(() => import('./components/DynamicComponents/Main Page/MainPage')) 
+const AboutUs = React.lazy(() => import('./components/StaticComponents/AboutUs/AboutUs')) 
 
 
 function App() {
