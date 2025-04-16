5bd1fe409d27380a433dc752c16082792d21db41
Leemorand1410 <leemorand1410@gmail.com>
2025-04-16 16:29:08 -0600

    fix: Update import paths in MainPage component for consistency
    fix: returning the finctional Rating page

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,8 +1,8 @@
-import "./MainPage.css"
-import News from '../News/News'
-import Ranking from '../Ranking/Ranking'
-import Videos from '../Videos/Videos'
-import IntroBanner from "../IntroBanner/IntroBanner"
+import "styles/MainPage.css"
+import News from 'components/DynamicComponents/News/News'
+import Ranking from 'components/DynamicComponents/Ranking/Ranking'
+import Videos from 'components/DynamicComponents/Videos/Videos'
+import IntroBanner from "components/DynamicComponents/IntroBanner/IntroBanner"
 
 
 export default function MainPage(){
